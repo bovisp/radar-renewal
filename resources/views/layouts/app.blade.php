e35b6957eb7e34ed46925b0c12ff0a5b9e414d2d
Paul Bovis <paul.bovis@canada.ca>
2018-06-11 17:43:35 -0600

    Fix missing icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
         <title>Radar Renewal</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css" />
 
         <link rel="stylesheet" href="css/app.css">
     </head>
