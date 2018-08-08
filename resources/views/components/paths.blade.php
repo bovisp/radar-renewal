a503414d04214d687b1e3269320c2ffc9dfdd322
Paul Bovis <paulbovisblahblah@gmail.com>
2018-08-08 17:15:05 +0000

    Update paths.blade.php

--- resources/views/components/paths.blade.php
+++ resources/views/components/paths.blade.php
@@ -1,207 +1,166 @@
-<section class="ui container pt-16" id="paths">
-  <h2 class="flow-header is-centered pb-16">Recommended Learning Paths</h2>
 
-  <p class="flow-text is-light pb-16">In the listing below we are offering a few recommended learning paths that roughly correspond to different target audiences within MSC.  These learning paths offer you a sequence of courses  that allow you to master a topic to the level you need in small steps.  The learning paths listed below are intended only a s a guide,  this gives you a starting point and still offers you the freedom to tailor your learning to your individual learning needs or interests. </p>
 
-  <div class="ui stackable four column grid">
-    <div class="column flex items-stretch">
-      <div class="ui segments">
-        <div class="ui segment p-0 m-0">
-          <h3 class="ui icon header pt-4 px-4">
-            <i class="graduation cap icon"></i>
 
-            <div class="content">
-              Research Scientists and Interested Radar Users
-            </div>
-          </h3>
 
-          <div class="ui divider mb-0"></div>
 
-          <div class="ui very relaxed divided list mt-0">
-            <div class="item px-4" style="padding-top: 0.85714286em;">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
-            </div>
 
-            <div class="item is-muted px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
-            </div>
 
-            <div class="item is-muted px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison (Optional)</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
-            </div>
 
-            <div class="item is-muted px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar (Optional)</a>
-            </div>
 
-            <div class="item is-muted px-4" style="padding-bottom: 0.85714286em;">
-              Radar Renewal: Dual-Polarization Advanced Products (Optional)
-            </div>
-          </div>
-        </div>
 
-        <div class="ui segment completion-time">
-            <strong>Time to complete:</strong> 1.5-5 hours
-        </div>
-      </div>
-    </div>
 
-    <div class="column flex items-stretch">
-      <div class="ui segments">
-        <div class="ui segment p-0 m-0">
-          <h3 class="ui icon header pt-4 px-4">
-            <i class="user secret icon"></i>
 
-            <div class="content">
-              Warning Preparedness Meteorologists
-            </div>
-          </h3>
 
-          <div class="ui divider mb-0"></div>
 
-          <div class="ui very relaxed divided list mt-0">
-            <div class="item is-muted px-4" style="padding-top: 0.85714286em;">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher (Optional)</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
-            </div>
 
-            <div class="item is-muted px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
-            </div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher</a>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
-            </div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
 
-            <div class="item is-muted px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar (Optional)</a>
-            </div>
 
-            <div class="item is-muted px-4" style="padding-bottom: 0.85714286em;">
-              Radar Renewal: Dual-Polarization Advanced Products (Optional)
-            </div>
-          </div>
-        </div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
 
-        <div class="ui segment completion-time">
-          <strong>Time to complete:</strong> 1.5-2 hours
-        </div>
-      </div>
-    </div>
 
-    <div class="column flex items-stretch">
-      <div class="ui segments">
-        <div class="ui segment p-0 m-0">
-          <h3 class="ui icon header pt-4 px-4 w-full pb-6">
-            <i class="cloud icon"></i>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison (Optional)</a>
 
-            <div class="content">
-              Operational Forecasters
-            </div>
-          </h3>
 
-          <div class="ui divider mb-0"></div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
 
-          <div class="ui very relaxed divided list mt-0">
-            <div class="item is-muted px-4" style="padding-top: 0.85714286em;">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher (Optional)</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
-            </div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar (Optional)</a>
 
-            <div class="item is-muted px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar</a>
-            </div>
 
-            <div class="item is-muted px-4" style="padding-bottom: 0.85714286em;">
-              Radar Renewal: Dual-Polarization Advanced Products (Optional)
-            </div>
-          </div>
-        </div>
 
-        <div class="ui segment completion-time">
-          <strong>Time to complete:</strong> 3-4 hours
-        </div>
-      </div>
-    </div>
 
-    <div class="column flex items-stretch">
-      <div class="ui segments">
-        <div class="ui segment p-0 m-0">
-          <h3 class="ui icon header pt-4 px-4  pb-6">
-            <i class="bolt icon"></i>
 
-            <div class="content">
-              Severe Weather Forecasters
-            </div>
-          </h3>
 
-          <div class="ui divider mb-0"></div>
 
-          <div class="ui very relaxed divided list mt-0">
-            <div class="item is-muted px-4" style="padding-top: 0.85714286em;">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher (Optional)</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
-            </div>
 
-            <div class="item is-muted px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
-            </div>
 
-            <div class="item px-4">
-              <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar</a>
-            </div>
 
-            <div class="item px-4" style="padding-bottom: 0.85714286em;">
-              Radar Renewal: Dual-Polarization Advanced Products
-            </div>
-          </div>
-        </div>
 
-        <div class="ui segment completion-time">
-          <strong>Time to complete:</strong> 3-4 hours
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher (Optional)</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar (Optional)</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher (Optional)</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=147">Radar Renewal: Radar Refresher (Optional)</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=154">Radar Renewal: S-Band</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=145">Renewal: X-Band (Optional)</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=144">Radar Renewal: S/C/X-Band Comparison</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=157">Radar Renewal: Dual-Polarization Fundamentals</a>
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=148">Radar Renewal: Dual-Polarization Basic Products Radar</a>
+
+
+
+
+
+
+
+
+
+
+
+
