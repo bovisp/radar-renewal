3dbf55a85d960c74d77f2250fc52708a6dfef4c3
Paul Bovis <paulbovisblahblah@gmail.com>
2018-08-08 17:13:13 +0000

    Update courses.blade.php

--- resources/views/components/courses.blade.php
+++ resources/views/components/courses.blade.php
@@ -1,191 +1,154 @@
-<section class="ui container" id="courses">
-  <h2 class="flow-header is-centered pb-16">Courses</h2>
 
-  <div class="ui stackable three column grid">
-    <div class="column flex items-stretch">
-      <div class="ui card">
-        <div class="image">
-          <img src="images/radar_refresher.jpg" alt="Radar Renewal: Radar Refresher image">
-        </div>
 
-        <div class="content">
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=147" class="header" title="Register for this course now">Radar Renewal: Radar Refresher</a>
 
-          <div class="meta">
-            <strong>Estimated time to complete:</strong> 15 minutes - 4 hours
-          </div>
 
-          <div class="description">
-            This <strong>optional</strong> course enables forecasters to refresh their knowledge of existing operational weather radars.  A number of resources have been included for radar fundamentals and software knobology.
-          </div>
-        </div>
 
-        <div class="extra content flex items-center">
-          {{-- <i class="users icon" style="position: relative; top:-3px;"></i> <span>17 Registered</span> --}}
 
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=147" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
-        </div>
-      </div>
-    </div>
 
-    <div class="column flex items-stretch">
-      <div class="ui card">
-        <div class="image">
-          <img src="images/s_band.jpg" alt="Radar Renewal: S-Band image">
-        </div>
 
-        <div class="content">
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=154" class="header" title="Register for this course now">Radar Renewal: S-Band</a>
 
-          <div class="meta">
-            <strong>Estimated time to complete:</strong> 25-40 minutes
-          </div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=147" class="header" title="Register for this course now">Radar Renewal: Radar Refresher</a>
 
-          <div class="description">
-            This course discusses the main advantages of a 10 cm wavelength radar as well as the limitations encountered with some of the Selex 1700s characteristics. It concludes by presenting the new radar scanning strategy underlying the many aspects that were taking under consideration.
-          </div>
-        </div>
 
-        <div class="extra content flex items-center">
-          {{-- <i class="users icon" style="position: relative; top:-3px;"></i> <span>34 Registered</span> --}}
 
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=154" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
-        </div>
-      </div>
-    </div>
 
-    <div class="column flex items-stretch">
-      <div class="ui card">
-        <div class="image">
-          <img src="images/x_band.jpg" alt="Radar Renewal: X-Band image">
-        </div>
 
-        <div class="content">
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=145" class="header" title="Register for this course now">Radar Renewal: X-Band</a>
 
-          <div class="meta">
-            <strong>Estimated time to complete:</strong> 20-30 minutes
-          </div>
 
-          <div class="description">
-            This course will provide an overview of X-Band radars in a Canadian context.  This includes the basic characteristics of  a 3 cm wavelength radar as well as the strengths and limitations.
-          </div>
-        </div>
 
-        <div class="extra content flex items-center">
-          {{-- <i class="users icon" style="position: relative; top:-3px;"></i> <span>12 Registered</span> --}}
 
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=145" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="ui stackable three column grid">
-    <div class="column flex items-stretch">
-      <div class="ui card">
-        <div class="image">
-          <img src="images/s_c_x_band_comparison.jpg" alt="Radar Renewal: S/C/X-Band Comparison image">
-        </div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=147" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
 
-        <div class="content">
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=144" class="header" title="Register for this course now">Radar Renewal: S/C/X-Band Comparison</a>
 
-          <div class="meta">
-            <strong>Estimated time to complete:</strong> 25-40 minutes
-          </div>
 
-          <div class="description">
-            This course will provide an overview of the key differences between the various weather radar systems in operation across Canada as well as highlighting aspects of the new S-Band systems that will be transparent and not-transparent to the end users.
-          </div>
-        </div>
 
-        <div class="extra content flex items-center">
-          {{-- <i class="users icon" style="position: relative; top:-3px;"></i> <span>29 Registered</span> --}}
 
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=144" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
-        </div>
-      </div>
-    </div>
 
-    <div class="column flex items-stretch">
-      <div class="ui card">
-        <div class="image">
-          <img src="images/dual_polarization_fundamentals.jpg" alt="Radar Renewal: Dual-Polarization Fundamentals image">
-        </div>
 
-        <div class="content">
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=157" class="header" title="Register for this course now">Radar Renewal: Dual-Polarization Fundamentals</a>
 
-          <div class="meta">
-            <strong>Estimated time to complete:</strong> 15-20 minutes
-          </div>
 
-          <div class="description">
-            This course introduces the concepts associated with Dual Polarization technology.  An overview of the benefits and the basic parameters of Dual Polarized Radars will also be provided.
-          </div>
-        </div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=154" class="header" title="Register for this course now">Radar Renewal: S-Band</a>
 
-        <div class="extra content flex items-center">
-          {{-- <i class="users icon" style="position: relative; top:-3px;"></i> <span>30 Registered</span> --}}
 
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=157" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
-        </div>
-      </div>
-    </div>
 
-    <div class="column flex items-stretch">
-      <div class="ui card">
-        <div class="image">
-          <img src="images/dual_polarization_basic_products.jpg" alt="Radar Renewal: Dual-Polarization Basic Radar Products image">
-        </div>
 
-        <div class="content">
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=148" class="header" title="Register for this course now">Radar Renewal: Dual-Polarization Basic Radar Products</a>
 
-          <div class="meta">
-            <strong>Estimated time to complete:</strong> 55 minutes - 1.5 hours
-          </div>
 
-          <div class="description">
-            This course will provide an overview of the basic dual-polarization products such as Differential Reflectivity, Correlation Coefficient, and Differential Phase Shift.
-          </div>
-        </div>
 
-        <div class="extra content flex items-center">
-          {{-- <i class="users icon" style="position: relative; top:-3px;"></i> <span>15 Registered</span> --}}
 
-          <a href="http://wto-science-moodle.to.on.ec.gc.ca/moodle/course/view.php?id=148" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="ui stackable centered three column grid">
-    <div class="column flex items-stretch">
-      <div class="ui card">
-        <div class="image">
-          <img src="images/dual_polarization_advanced_products.jpg" alt="Radar Renewal: Dual-Polarization Advanced Products image">
-        </div>
 
-        <div class="content">
-          <div class="header" title="Register for this course now">Radar Renewal: Dual-Polarization Advanced Products</div>
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=154" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
 
-          <div class="meta">
-            <strong>Estimated time to complete:</strong> N/A
-          </div>
 
-          <div class="description">
-            This course will provide an overview of the advanced dual-polarization products such as Particle Classification (PCA) and Quantitative Precipitation Estimation (QPE).
-          </div>
-        </div>
 
-        <div class="extra content flex items-center">
-          {{-- <i class="users icon" style="position: relative; top:-3px;"></i> <span>23 Registered</span> --}}
 
-          <a href="#" class="blue basic ui button small ml-auto disabled" title="Register now">Coming soon</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=145" class="header" title="Register for this course now">Radar Renewal: X-Band</a>
+
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=145" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
+
+
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=144" class="header" title="Register for this course now">Radar Renewal: S/C/X-Band Comparison</a>
+
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=144" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=157" class="header" title="Register for this course now">Radar Renewal: Dual-Polarization Fundamentals</a>
+
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=157" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=148" class="header" title="Register for this course now">Radar Renewal: Dual-Polarization Basic Radar Products</a>
+
+
+
+
+
+
+
+
+
+
+http://msc-educ-smc.cmc.ec.gc.ca/moodle/course/view.php?id=148" class="blue basic ui button small ml-auto" title="Register now">Register now</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
